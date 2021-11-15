be2c72de70c16b57bc27d971420098946391ee5b
devsantostiago <dev.santostiago@gmail.com>
2021-11-15 22:45:00 +0100

    Added homebrew dependencies installation function

--- install.sh
+++ install.sh
@@ -5,8 +5,14 @@ install_homebrew () {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
+install_brew_dependencies () {
+  echo "Installing Brew Dependencies"
+  brew bundle --file=brew/Brewfile || echo "INFO: Some applications were already installed."
+}
+
 main () {
    install_homebrew 
+   install_brew_dependencies
 }
 
 main "$@"
