873f1f96e2f526e0d61023d5ea01a656f59f1d34
devsantostiago <dev.santostiago@gmail.com>
2021-11-16 00:10:59 +0100

    Define pipefail in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 install_homebrew () {
   echo "Installing Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
